6e15ec96e9d508d6283b240503573ee3c8d4c650
SarahC <38871276+sarah-colq@users.noreply.github.com>
2018-10-09 00:17:14 -0400

    Basic html parser added

--- scrape_data.py
+++ scrape_data.py
@@ -1,9 +1,20 @@
 import sys
-import BeautifulSoup4
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
 
+
+def scrape_cwgc():
+    url = "https://www.cwgc.org/find/find-war-dead/results?war=1&servedWith=Canadian"
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content,'lxml')
+    table = soup.find(name='table')
+    # result = pd.DataFrame([[td.text for td in row.findAll('td')] for row in table.tbody.findAll('tr')])
+    # print(result)
 
 def main():
-    print(1)
+    scrape_cwgc()
+    print('Done')
 
 if __name__ == "__main__":
 	sys.exit(main())
